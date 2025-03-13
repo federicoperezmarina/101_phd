a75b4c18693af48e0e5175cbe20a3d2a57218c38
Federico Perez Marina <federicoperezmarina@MacBook-Pro-de-Federico.local>
2025-03-13 17:08:36 +0100

    feature: ollama working on local

--- code/lm_studio.py
+++
@@ -1,8 +0,0 @@
-import lmstudio as lms
-
-llm = lms.llm() # Get any loaded LLM
-
-prediction = llm.respond_stream("What is a Capybara?")
-
-for token in prediction:
-    print(token, end="", flush=True)
